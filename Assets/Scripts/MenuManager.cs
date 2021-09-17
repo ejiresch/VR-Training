e13a49017ff448b2c8a50b782a9e459cba054c65
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2021-09-17 13:14:17 +0200

    Scene "MainMenu" created

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace b
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public TextMeshPro text;
+        public void PressButton()
+        {
+            text.SetText("change");
+        }
+    }
+}
