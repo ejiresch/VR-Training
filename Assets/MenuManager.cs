d6ab695662d7f20dbf81be3dad8f390b492f74e3
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2021-09-17 13:35:48 +0200

    Testarea in Scene "MainMenu" eingefügt

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PressButton()
+    {
+        TextMeshPro text = GetComponentInChildren<TextMeshPro>();
+        text.SetText("Button clicked");
+    }
+}
