20dc38bb6dce4285306717b84fe8a28545cf2e14
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2021-10-11 13:48:56 +0200

    Debug Panel hinzugefügt

---
+++ Assets/Scripts/UserInterface/TestPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPanel : MonoBehaviour
+{
+    public void pressButton0()
+    {
+
+    }
+    public void pressButton1()
+    {
+
+    }
+    public void pressButton2()
+    {
+
+    }
+    public void pressButton3()
+    {
+
+    }
+    public void pressButton4()
+    {
+
+    }
+    public void pressButton5()
+    {
+
+    }
+}
