93fb24e042b95c7994912b12bc082e0b567b6688
mbrsanovic <57634625+mbrsanovic@users.noreply.github.com>
2021-10-11 14:13:46 +0200

    Sure

--- Assets/Scripts/UserInterface/TestPanel.cs
+++ Assets/Scripts/UserInterface/TestPanel.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestPanel : MonoBehaviour
 {
-    public void pressButton0()
+    public void ResetButton()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void pressButton1()
     {
