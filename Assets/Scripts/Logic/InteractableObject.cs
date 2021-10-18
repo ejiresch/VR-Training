eac60f3db9fe3116a6eb4d903ebf28f49a0c2fdf
mbrsanovic <57634625+mbrsanovic@users.noreply.github.com>
2021-10-18 14:05:26 +0200

    Snap zones

--- Assets/Scripts/Logic/InteractableObject.cs
+++ Assets/Scripts/Logic/InteractableObject.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 // Objects that have this script, send reports upon colliding with other interactibles
-public class CollisionReport : MonoBehaviour
-{   
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] private bool isGrabbed = false;
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.layer == 9)
@@ -11,5 +14,15 @@ public class CollisionReport : MonoBehaviour
             CollisionEvent ce = new CollisionEvent(this.gameObject, collision.gameObject);
             ce.ReportCollision();
         }
+    }
+
+    public bool GetIsGrabbed()
+    {
+        return isGrabbed;
+    }
+
+    public void SetIsGrabbed(bool isg)
+    {
+        this.isGrabbed = isg;
     }
 }
