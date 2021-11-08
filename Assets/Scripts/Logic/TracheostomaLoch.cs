cd62d2bff7567d1541add9314cd48a5ecaaaf352
mbrsanovic <57634625+mbrsanovic@users.noreply.github.com>
2021-11-08 14:35:19 +0100

    Tasks for prototype scene

--- Assets/Scripts/Logic/TracheostomaLoch.cs
+++ Assets/Scripts/Logic/TracheostomaLoch.cs
@@ -9,4 +9,10 @@ public class TracheostomaLoch : MonoBehaviour
         RotationCollisionEvent collisionEvent = new RotationCollisionEvent(this.gameObject, collision.gameObject, collision.gameObject.transform.rotation.eulerAngles);
         collisionEvent.ReportCollision();
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CollisionEvent ce = new CollisionEvent(this.gameObject, other.gameObject);
+        ce.ReportCollision();
+    }
 }
