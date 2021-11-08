5fed20faccc93aa88bc374c6c33db7fb8288352b
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2021-11-08 13:21:05 +0100

    TV Screen + Script implemented

---
+++ Assets/Scripts/UserInterface/TVHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVHandler : MonoBehaviour
+{
+    public void startVideo(int index)
+    {
+        // start video with index
+    }
+    public void stopVideo()
+    {
+        // stop video
+    }
+    public void resumeVideo()
+    {
+        // resume video
+    }
+}
