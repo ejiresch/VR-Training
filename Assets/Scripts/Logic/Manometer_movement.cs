683ec874aaf8fba97bea809b6b6a2e9e1d152269
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2021-12-15 14:50:47 +0100

    XR Interaction Toolkit implementiert

---
+++ Assets/Scripts/Logic/Manometer_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Manometer_movement : MonoBehaviour
+{
+    public GameObject nadel;
+    public InputActionReference toggleRef = null;
+
+    private void Awake()
+    {
+        toggleRef.action.started += Toggle;
+    }
+
+    private void OnDestroy()
+    {
+        toggleRef.action.started -= Toggle;
+
+    }
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        bool isActive = !gameObject.activeSelf;
+        gameObject.SetActive(isActive);
+    }
+
+    
+}
