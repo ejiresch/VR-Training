87df0a92fdd85cd7733a1665fc08c7877e306501
tgm-kstary <57392948+tgm-kstary@users.noreply.github.com>
2022-01-16 18:14:33 +0100

    Oculus Integration Asset

---
+++ Assets/UserFeedback/VibrationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationManager : MonoBehaviour
+{
+    OVRHapticsClip clip = new OVRHapticsClip();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0; i<2; i++)
+        {
+            clip.WriteSample(i % 50 == 0 ? (byte)255 : (byte)0);
+        }
+        OVRHaptics.LeftChannel.Preempt(clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
