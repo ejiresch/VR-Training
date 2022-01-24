567d49c985fcd58c517fddbde0d1798f1459a8f2
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2022-01-24 14:49:44 +0100

    HandHandler implemented

---
+++ Assets/Scripts/UserInterface/HandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+
+public class HandHandler : MonoBehaviour
+{
+    public GameObject controls; // on default disabled
+    public GameObject right_ray_interactor; // on default enabled
+    public GameObject left_ray_interactor; // on default enabled
+    public InputActionReference toggleReferenceControl_right = null;
+    public InputActionReference toggleReferenceControl_left = null;
+
+    private void Awake()
+    {
+        toggleReferenceControl_right.action.started += ShowControls;
+        toggleReferenceControl_left.action.started += ShowControls;
+    }
+
+    private void OnDestroy()
+    {
+        toggleReferenceControl_right.action.started -= ShowControls;
+        toggleReferenceControl_left.action.started -= ShowControls;
+    }
+    public void ShowControls(InputAction.CallbackContext context)
+    {
+        bool isActive = !controls.activeSelf;
+        controls.SetActive(isActive);
+
+        var right_hand_ray = right_ray_interactor.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>();
+        right_hand_ray.gameObject.SetActive(!isActive);
+
+        var left_hand_ray = left_ray_interactor.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>();
+        left_hand_ray.gameObject.SetActive(!isActive);
+    }
+}
