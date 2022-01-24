237ef975a9b0818b6fbb6b9be8ea6bde1890303b
Luca Bauer <lbauer@student.tgm.ac.at>
2022-01-24 14:54:31 +0100

    Kanuele Cuffanimation

---
+++ Assets/KanueleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanueleController : MonoBehaviour
+{
+
+    private Animator a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetButtonDown("Submit"))
+        {
+            a.SetTrigger("auslassen");
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            a.SetTrigger("aufblasen");
+        }
+
+    }
+}
