233aca6fc8acba0b7ea110c3d07600496a2bff70
tgm-kstary <57392948+tgm-kstary@users.noreply.github.com>
2022-02-02 10:17:53 +0100

    ColliderVibration

---
+++ Assets/Scripts/HapticFeedback/ColliderVibration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColliderVibration : MonoBehaviour
+{
+    public GameObject rechterController;
+    public GameObject linkerController;
+    XRController rechts;
+    XRController links;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rechts = rechterController.GetComponent<XRController>();
+        links = linkerController.GetComponent<XRController>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        float distanceRight = Vector3.Distance(other.transform.position, rechterController.transform.position);
+        float disanceLeft = Vector3.Distance(other.transform.position, linkerController.transform.position);
+        if (distanceRight < disanceLeft)
+        {
+            rechts.SendHapticImpulse(0.4f, 0.1f);
+        }
+        else
+        {
+            links.SendHapticImpulse(0.4f, 0.1f);
+        }
+    }
+}
