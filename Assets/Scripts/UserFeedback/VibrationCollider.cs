8100cb4e734ebf625d9c765b4e066145d2762c20
tgm-kstary <57392948+tgm-kstary@users.noreply.github.com>
2022-02-02 10:17:25 +0100

    VibrationColliderCube

---
+++ Assets/Scripts/UserFeedback/VibrationCollider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VibrationCollider : MonoBehaviour
+{
+    public GameObject rechterController;
+    public GameObject linkerController;
+    XRController rechts;
+    XRController links;
+
+    private void Start()
+    {
+        rechts = rechterController.GetComponent<XRController>();
+        links = linkerController.GetComponent<XRController>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        float distanceRight = Vector3.Distance(other.transform.position, rechterController.transform.position);
+        float disanceLeft = Vector3.Distance(other.transform.position, linkerController.transform.position);
+        if (distanceRight < disanceLeft)
+        {
+            rechts.SendHapticImpulse(0.4f, 0.1f);
+        }
+        else
+        {
+            links.SendHapticImpulse(0.4f, 0.1f);
+        }
+    }
+}
