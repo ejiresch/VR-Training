7f665c68d9acfe8073dece143bf225bbe933137c
Hadinger-tgm <fhadinger@student.tgm.ac.at>
2022-03-07 13:05:06 +0100

    spitze animation hinzugefügt + kleinere Änderungen

--- Assets/Scripts/Logic/Task/SpritzePressObject.cs
+++ Assets/Scripts/Logic/Task/SpritzePressObject.cs
@@ -8,6 +8,7 @@ public class SpritzePressObject : PressObject
     public InputActionReference toggleReferenceLeft = null;
     public InputActionReference toggleReferenceRight = null;
     public bool reingepumpt = false;
+    public GameObject kolben;
 
     private void Awake()
     {
@@ -26,15 +27,45 @@ public class SpritzePressObject : PressObject
         {
             //  hier wird reingepumpt
             reingepumpt = true;
+            StartCoroutine(Reinpumpen());
         }
         else
         {
             //  hier wird rausgepumpt und task beendet
+            StartCoroutine(Rauspumpen());
             Press();
             GetComponent<ConnectorObject>().Disconnect();
             GameObject temp = GameObject.FindGameObjectWithTag("CompoundGrabbablePart").transform.parent.parent.gameObject;
             temp.GetComponent<InteractableObject>().SetGrabbable(true);
             temp.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+    IEnumerator Reinpumpen()
+    {
+
+        if (this.GetComponent<InteractableObject>().GetIsGrabbed())
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                kolben.transform.localPosition -= new Vector3(0, 0.05f, 0);
+                if (kolben.transform.localPosition.y < 6) { break; }
+                yield return new WaitForSeconds(0.008f);
+            }
+            reingepumpt = true;
+        }
+    }
+    IEnumerator Rauspumpen()
+    {
+        if (this.GetComponent<InteractableObject>().GetIsGrabbed())
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                kolben.transform.position += new Vector3(0, 0.05f, 0);
+                if (kolben.transform.position.y > 9) { break; }
+                yield return new WaitForSeconds(0.008f);
+            }
+            Press();
+            GetComponent<ConnectorObject>().Disconnect();
         }
     }
 }
