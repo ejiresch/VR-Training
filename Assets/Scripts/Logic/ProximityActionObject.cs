1e5bb809ec2bb1d8933db716a7ea2da1e27453c6
Marvin Fornezzi <56915960+mfornezzi-tgm@users.noreply.github.com>
2022-12-13 11:18:45 +0100

    Statisches Glas/Mörser umgesetzt (ProximityAction / Task)

---
+++ Assets/Scripts/Logic/ProximityActionObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+// Ein Objekt, welche bei der Beruehrung zweier Objekte die Task beendet
+public class ProximityActionObject : InteractableObject
+{
+    public InputActionReference toggleReference = null;
+    private GameObject touchTarget;
+    private bool inRange = false;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+    // Setzt das Ziel des TouchObjects
+    void FixedUpdate()
+    {
+        if (touchTarget != null)
+        {
+            inRange = Vector3.Distance(touchTarget.transform.position, this.transform.position) < 0.1f;
+        }
+    }
+    public void SetTouchTarget(GameObject touchTarger) => touchTarget = touchTarger;
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        if (inRange)
+        {
+            StartCoroutine(EventAnimation());
+            ProcessHandler.Instance.NextTask();
+        }
+    }
+    IEnumerator EventAnimation()
+    {
+        anim.SetTrigger("reinpumpen");
+        yield return new WaitForSeconds(1.3f);
+    }
+    public void SetTarget(GameObject touchTarger) => touchTarget = touchTarger;
+    private void Awake() => toggleReference.action.started += Toggle;
+
+    private void OnDestroy() => toggleReference.action.started -= Toggle;
+}
---
+++ Assets/Scripts/Logic/Task/ProximityActionTask.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Task, welche bei Beruehrung zweier Objekte beendet wird
+public class ProximityActionTask : Task
+{
+    public GameObject touchObject, touchTarget;
+    // Wird bei Start der Task ausgefuehrt
+    public override void StartTask()
+    {
+        base.StartTask();
+        touchObject = base.FindTool(touchObject.name);
+        touchTarget = base.FindTool(touchTarget.name);
+        touchObject.GetComponent<ProximityActionObject>().SetTarget(touchTarget);
+    }
+}
