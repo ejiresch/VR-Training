c7934634ceff2308c1231cb0c5c5b1d9f8da0cda
sstangelberger <sstangelberger@student.tgm.ac.at>
2022-12-20 15:14:33 +0100

    ActiveButtonChanged, Neu Beginnen changed

---
+++ Assets/ActiveProcessButtonChange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveProcessButtonChange : MonoBehaviour
+{
+    public SceneLoader scene;
+    public ProcessScriptableObject process;
+    public Color highlightedColor = new Color(0.3f, 1, 0.3f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (process.pName.Equals(scene.selectedProcess.pName))
+        { 
+            var colors = gameObject.GetComponent<Button>().colors;
+            colors.normalColor = highlightedColor;
+            colors.pressedColor = highlightedColor;
+            colors.selectedColor = highlightedColor;
+            colors.highlightedColor = highlightedColor;
+            this.gameObject.GetComponent<Button>().colors = colors;
+        } 
+        /**else
+        {
+            var colors = this.gameObject.GetComponent<Button>().colors;
+            colors.normalColor = Color.white;
+            colors.pressedColor = Color.white;
+            colors.selectedColor = Color.white;
+            colors.highlightedColor = Color.white;
+            this.gameObject.GetComponent<Button>().colors = colors;
+        }*/
+
+
+    }
+}
