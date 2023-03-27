0e89d105a0a353bfed0680c7b90f06d5a6fa23bf
Marvin Fornezzi <56915960+mfornezzi-tgm@users.noreply.github.com>
2023-03-27 09:27:30 +0200

    HandschuhTask hinzugefügt

--- Assets/Scripts/Logic/Task/HandTouchTask.cs
+++ Assets/Scripts/Logic/Task/HandTouchTask.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class HandTouchTask : Task
 {
     public GameObject touchTarget;
+    private bool active = false;
     // Wird bei Start der Task ausgefuehrt
     public override void StartTask()
     {
         base.StartTask();
         touchTarget = base.FindTool(touchTarget.name);
-        //touchObject.GetComponent<TouchObject>().SetTouchTarget(touchTarget);
+        active = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (active)
+        {
+            GameObject[] touchObjects = GameObject.FindGameObjectsWithTag("touchHand");
+            if (touchObjects != null && touchObjects.Length>1)
+            {
+                Debug.Log(touchObjects.Length);
+                foreach (GameObject touchObject in touchObjects)
+                {
+                    touchObject.GetComponent<TouchHand>().SetTouchTarget(touchTarget);
+                    touchObject.GetComponent<TouchHand>().SetHands(new List<GameObject>() { touchObjects[0], touchObjects[1] });
+                }
+                active = false;
+            }
+        }
     }
 }
