2dfb9493133260a4894e3ff247517433d33c073b
BurgstallerR <rburgstaller@student.tgm.ac.at>
2023-11-05 19:08:02 +0100

    Med Crush functional
    PEG Med funktioniert, momentan animation nur farbveränderung des Medikamentes

---
+++ Assets/Scripts/Logic/Task/MedPressObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+
+/* Ist für die Med Crush Animation zuständig */
+[RequireComponent(typeof(MaterialFetcher))]
+public class MedPressObject : PressObject, ResetInterface
+{
+    public InputActionReference toggleReference = null;
+    [HideInInspector]public bool crushed = false; 
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();  
+    }
+
+
+    private void Awake() => toggleReference.action.started += Toggle;
+
+    private void OnDestroy() => toggleReference.action.started -= Toggle;
+    private void Toggle(InputAction.CallbackContext context) // Wird aufgerufen, wenn der Button für toggleReference gedrückt wird -> siehe Samples/Default Input Actions/XRI Default Input Actions
+    {
+        if (GetIsGrabbed())
+        {
+            if (!pressable) return;
+            if (!crushed) StartCoroutine(Crush());
+        }
+    }
+    public override void Press()
+    {
+        if (!pressable) return;
+        
+        base.Press();   //next task
+        
+    }
+    IEnumerator Crush() // Start der "Crush" Animation
+    {
+       
+        anim.SetTrigger("crushed");
+        yield return new WaitForSeconds(1.3f);
+        crushed = true;
+        Press();
+    }
+    public void ResetComp()
+    {
+        
+    }
+}
