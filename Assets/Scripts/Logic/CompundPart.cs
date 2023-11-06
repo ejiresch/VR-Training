fce2f56165d6f35c237d894a2c6533d77c761f0e
jakobwaldhauser <jwaldhauser@student.tgm.ac.at>
2023-11-06 13:02:57 +0100

    Tracheostoma entfernen BUGFIX

--- Assets/Scripts/Logic/CompundPart.cs
+++ Assets/Scripts/Logic/CompundPart.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 // Stellt ein Teil eines CompoundObjects dar
 public class CompundPart : InteractableObject
 {
     [SerializeField] private bool taskFocus;
+    
     // Start is called before the first frame update
     void Awake()
     {
         taskFocus = false;
+        GetComponent<XRGrabInteractable>().enabled = false;
     }
     // Wird aufgerufen wenn das Objekt losgelassen wird
     public override void OnDrop()
@@ -16,11 +19,28 @@ public class CompundPart : InteractableObject
         base.OnDrop();
         if (taskFocus)
         {
+            
             taskfinished = true;
             taskFocus = false;
             GetComponent<Rigidbody>().isKinematic = false;
+            
         }
     }
+
+    //ACHTUNG ACHTUNG BISSCHEN SO AUF TEST
+    public void GrabableUpdate()
+    {
+        if (taskFocus)
+        {
+            GetComponent<XRGrabInteractable>().enabled = true;
+        }
+    }
+
+
     // Ob das Objekt aktiv sein soll, soll von der Task gesetzt werden
-    public void SetTaskFocus(bool focus) => taskFocus = focus;
+    public void SetTaskFocus(bool focus)
+    {
+        taskFocus = focus;
+        GrabableUpdate();
+    }
 }
