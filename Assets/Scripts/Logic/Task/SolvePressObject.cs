ee090f04beba765dd8dc6a3f113138e1d512c5c5
BurgstallerR <rburgstaller@student.tgm.ac.at>
2023-11-27 20:40:58 +0100

    Med_solve grob Funktionsfähig
    Die Grundstruktur von Med_solve implementiert (das Destroyen des Medikament GO fehlt noch)

---
+++ Assets/Scripts/Logic/Task/SolvePressObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+
+/* Ist für die Med Crush Animation zuständig */
+public class SolvePressObject : PressObject, ResetInterface
+{
+    public InputActionReference toggleReference = null;
+    [HideInInspector]public bool crushed = false; 
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();  
+    }
+
+
+    private void Awake() => toggleReference.action.started += Toggle;
+
+    private void OnDestroy() => toggleReference.action.started -= Toggle;
+    private void Toggle(InputAction.CallbackContext context) // Wird aufgerufen, wenn der Button für toggleReference gedrückt wird -> siehe Samples/Default Input Actions/XRI Default Input Actions
+    {
+        if (GetIsGrabbed())
+        {
+            if (!pressable) return;
+            if (!crushed) StartCoroutine(Solve());
+        }
+    }
+    public override void Press()
+    {
+        if (!pressable) return;
+        
+        base.Press();   //next task
+        
+    }
+    IEnumerator Solve() // Start der "Solve" Animation
+    {
+       
+        anim.SetTrigger("solved");
+        GameObject go = GameObject.Find("Medikament");
+        if (go != null)
+        {
+            Destroy(go);
+        }
+        yield return new WaitForSeconds(1.3f);
+        crushed = true;
+        Press();
+    }
+    public void ResetComp()
+    {
+        
+    }
+}
