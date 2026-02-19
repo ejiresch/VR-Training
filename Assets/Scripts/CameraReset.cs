c89608cc81ea352f5cba003c58ac90d7ec6cf355
Tobias Salomon <tsalomon@student.tgm.ac.at>
2026-02-19 14:49:52 +0100

    Documented the code

--- Assets/Scripts/CameraReset.cs
+++ Assets/Scripts/CameraReset.cs
@@ -5,35 +5,47 @@ using UnityEngine.XR;
 
 public class CameraReset : MonoBehaviour
 {
+    // what characteristic to check for
     public InputDeviceCharacteristics controllerCharacteristics;
-    public Transform cameraTransform; // Assign Main Camera here
 
+    //transform of main Camera
+    public Transform cameraTransform;
+
+    //Input device wich it gets data from
     private InputDevice _targetDevice;
+
+    //postion to where it recenters
     private Vector3 _startPosition;
 
     private bool _wasPressed = false;
 
     void Start()
     {
+        //saves start position and gets devices
         _startPosition = transform.position;
         InitializeDevice();
     }
 
     void Update()
     {
+
+        //checks if targetDevice is valid -> if not then reinintialize
         if (!_targetDevice.isValid)
         {
             InitializeDevice();
             return;
         }
 
+        //gets input from secondary button and saves it into a tmp bool
         if (_targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool pressed))
         {
+            //checks if button was pressed and not pressed the frame earlier
             if (pressed && !_wasPressed)
             {
                 Recenter();
             }
 
+            //makes sure you don't get reset multiple times in a short time frame
             _wasPressed = pressed;
         }
     }
@@ -43,10 +55,12 @@ public class CameraReset : MonoBehaviour
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
+        /*
         foreach (var device in devices)
         {
             Debug.Log(device.name + "\n");
         }
+        */
 
         if (devices.Count > 0)
         {
@@ -54,6 +68,7 @@ public class CameraReset : MonoBehaviour
         }
     }
 
+    //Recenters the user
     void Recenter()
     {
         // How far the head moved inside tracking space
@@ -62,6 +77,6 @@ public class CameraReset : MonoBehaviour
         // Move XR Origin opposite to head offset
         transform.position = _startPosition - new Vector3(headOffset.x, 0, headOffset.z);
 
-        Debug.Log("Recentered XR Origin");
+        //Debug.Log("Recentered XR Origin");
     }
 }
