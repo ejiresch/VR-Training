254741fe3692b7ed7c91820f36123e0ea04f51af
WolfE2007 <ewolf@student.tgm.ac.at>
2026-03-05 13:54:17 +0100

    Changes
    Zoo, Museum and stuff

--- Assets/Scripts/Logic/spawnBack.cs
+++ Assets/Scripts/Logic/spawnBack.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class spawnBack : MonoBehaviour
 {
     public Transform spawnPoint;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
