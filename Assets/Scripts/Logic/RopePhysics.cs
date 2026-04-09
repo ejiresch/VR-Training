41666459c5ff9a39c8c225b88e44c79911342521
WolfE2007 <ewolf@student.tgm.ac.at>
2026-04-09 15:31:12 +0200

    Renamed files and fixed cuffline

--- Assets/Scripts/Logic/RopePhysics.cs
+++ Assets/Scripts/Logic/RopePhysics.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class RopePhysics : MonoBehaviour
 {
-
     public Transform startPoint;
     public Transform endPoint;
     public int ropesections = 15;
@@ -15,7 +14,6 @@ public class RopePhysics : MonoBehaviour
     private List<RopeNode> nodes = new List<RopeNode>();
     private LineRenderer lineRenderer;
 
-    // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
@@ -25,38 +23,81 @@ public class RopePhysics : MonoBehaviour
         for (int i = 0; i < ropesections; i++)
         {
             RopeNode n = new RopeNode(ropeNodePos);
-            ropeNodePos.y -= ropeNodeLength;
             nodes.Add(n);
+            ropeNodePos.y -= ropeNodeLength;
         }
-        endPoint.transform.parent.GetComponent<Rigidbody>().isKinematic = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         UpdateRopeSimulation();
 
+        // Fix first node to start point
         RopeNode firstNode = nodes[0];
         firstNode.pos = startPoint.position;
         nodes[0] = firstNode;
 
-        for (int i = 0; i < 60; i++)
+        // Apply constraints multiple times
+        for (int i = 0; i < 50; i++)
         {
             MaximumStretch();
         }
 
-        for (int i = 0; i < 20; i++)
-        {
-            AdjustCollision();
-        }
-
         DisplayRope();
+    }
 
-        RopeNode lastNode = nodes[nodes.Count - 1];
-        lastNode.pos = endPoint.position;
-        /*endPoint.position = lastNode.pos; */
-        nodes[nodes.Count - 1] = lastNode;
+    private void UpdateRopeSimulation()
+    {
+        Vector3 gravityVec = new Vector3(0f, gravity, 0f);
+        float t = Time.deltaTime;
+
+        for (int i = 1; i < nodes.Count; i++)
+        {
+            RopeNode current = nodes[i];
+
+            Vector3 velocity = current.pos - current.oldPos;
+            velocity *= 0.99f; // damping
+
+            current.oldPos = current.pos;
+            current.pos += velocity;
+            current.pos += gravityVec * t * t;
+
+            nodes[i] = current;
+        }
+    }
+
+    private void MaximumStretch()
+    {
+        for (int i = 0; i < nodes.Count - 1; i++)
+        {
+            RopeNode top = nodes[i];
+            RopeNode bot = nodes[i + 1];
+
+            float dist = Vector3.Distance(top.pos, bot.pos);
+            float error = dist - ropeNodeLength;
+
+            Vector3 changeDir = (top.pos - bot.pos).normalized;
+            Vector3 change = changeDir * error;
+
+            if (i == 0)
+            {
+                bot.pos += change;
+                nodes[i + 1] = bot;
+            }
+            else if (i + 1 == nodes.Count - 1 && endPoint != null)
+            {
+                bot.pos = endPoint.position;
+                nodes[i + 1] = bot;
+            }
+            else
+            {
+                bot.pos += change * 0.5f;
+                top.pos -= change * 0.5f;
+
+                nodes[i + 1] = bot;
+                nodes[i] = top;
+            }
+        }
     }
 
     private void DisplayRope()
@@ -75,95 +116,6 @@ public class RopePhysics : MonoBehaviour
         lineRenderer.SetPositions(positions);
     }
 
-    private void FixedUpdate()
-    {
-    }
-
-    private void UpdateRopeSimulation()
-    {
-        Vector3 gravity = new Vector3(0f, this.gravity, 0f);
-        float t = Time.deltaTime;
-
-        for (int i = 1; i < nodes.Count; i++)
-        {
-
-            RopeNode current = nodes[i];
-            Vector3 vel = current.pos - current.oldPos;
-            current.oldPos = current.pos;
-            Vector3 newPos = current.pos + vel;
-            newPos += gravity * t;
-            current.pos = newPos;
-            nodes[i] = current;
-        }
-    }
-
-    private void AdjustCollision()
-    {
-        for(int i = 2; i < nodes.Count; i++)
-        {
-
-            if (i == nodes.Count - 2)
-                break;
-
-            RopeNode current = nodes[i];
-
-            Collider[] colliders = Physics.OverlapSphere(current.pos, ropeWidth / 2f);
-
-            foreach (Collider c in colliders)
-            {
-                ////Debug.Log(c.transform.name);
-                Vector3 cCenter = c.transform.position;
-                Vector3 cDirection = cCenter - current.pos;
-                current.pos -= cDirection.normalized * 0.1f * Time.deltaTime;
-                nodes[i] = current;
-                break;
-            }
-        }
-    }
-
-    private void MaximumStretch()
-    {
-        for (int i = 0; i < nodes.Count - 1; i++)
-        {
-            RopeNode top = nodes[i];
-            RopeNode bot = nodes[i + 1];
-
-            float dist = (top.pos - bot.pos).magnitude;
-            float distError = Mathf.Abs(dist - ropeNodeLength);
-
-            Vector3 changeDir = Vector3.zero;
-
-            if (dist > ropeNodeLength)
-            {
-                changeDir = (top.pos - bot.pos).normalized;
-            }
-            else if (dist < ropeNodeLength)
-            {
-                changeDir = (bot.pos - top.pos).normalized;
-            }
-
-            Vector3 change = changeDir * distError;
-
-            if (i != 0 && i + 1 != nodes.Count - 1)
-            {
-                bot.pos += change * ropeNodeLength;
-                nodes[i + 1] = bot;
-                top.pos -= change * ropeNodeLength;
-                nodes[i] = top;
-            }
-            else if (i == 0)
-            {
-                bot.pos += change;
-                nodes[i + 1] = bot;
-            }
-            else
-            {
-                top.pos -= change;
-                nodes[i] = top;
-            }
-        }
-    }
-
     public struct RopeNode
     {
         public Vector3 pos;
@@ -174,6 +126,5 @@ public class RopePhysics : MonoBehaviour
             this.pos = pos;
             this.oldPos = pos;
         }
-
     }
 }
